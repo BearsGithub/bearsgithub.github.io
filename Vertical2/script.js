e580e374f9d67c3aede454190953902a33d30dda
Bryan Greenaway <Bryan.Greenaway@bears.nfl.net>
2019-11-10 10:01:08 -0600

    LAR game

--- Vertical2/script.js
+++ Vertical2/script.js
@@ -73,9 +73,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//PHI
+		//LAR
 		$("#countdown").countdown({
-			date: "03 November 2019 12:00:00",
+			date: "17 November 2019 19:20:00",
 			format: "on"
 		});
 	});
