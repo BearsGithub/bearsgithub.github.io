4f230ea97c06c5a4c93fc5093066264dd73a4cf2
Bryan Greenaway <54723602+BearsGithub@users.noreply.github.com>
2021-11-08 21:47:20 -0600

    BLT game

--- CurrentWeek/Horizontal/script.js
+++ CurrentWeek/Horizontal/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//PIT
+		//BLT
 		$("#countdown").countdown({
-			date: "8 November 2021 19:15:00",
+			date: "21 November 2021 12:00:00",
 			format: "on"
 		});
 	});
