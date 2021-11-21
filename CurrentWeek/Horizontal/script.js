f11225eadc5be9f0b628c5decc1ce29bd9ab6426
Bryan Greenaway <54723602+BearsGithub@users.noreply.github.com>
2021-11-21 14:04:03 -0600

    DET game

--- CurrentWeek/Horizontal/script.js
+++ CurrentWeek/Horizontal/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//BLT
+		//DET
 		$("#countdown").countdown({
-			date: "21 November 2021 12:00:00",
+			date: "25 November 2021 11:30:00",
 			format: "on"
 		});
 	});
