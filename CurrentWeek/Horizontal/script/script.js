43533406febd404887bef9f5524a71bb8a4c72a1
Bryan Greenaway <bryan.greenaway@bears.nfl.net>
2022-09-12 06:55:33 -0500

    actually GB this time

--- CurrentWeek/Horizontal/script/script.js
+++ CurrentWeek/Horizontal/script/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//HST
+		//GB
 		$("#countdown").countdown({
-			date: "25 September 2022 12:00:00",
+			date: "18 September 2022 19:20:00",
 			format: "on"
 		});
 	});
