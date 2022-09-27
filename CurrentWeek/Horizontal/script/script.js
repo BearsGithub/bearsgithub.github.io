7541747c588b559f15a42b21deb09150b6729738
BGreenaway <bryan.greenaway@bears.nfl.net>
2022-09-27 17:05:57 -0500

    NYG Game

--- CurrentWeek/Horizontal/script/script.js
+++ CurrentWeek/Horizontal/script/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//HST
+		//NYG
 		$("#countdown").countdown({
-			date: "25 September 2022 12:00:00",
+			date: "02 October 2022 12:00:00",
 			format: "on"
 		});
 	});
