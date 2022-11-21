34a83263fcf954b42a741db9ca675679ba672088
Bryan Greenaway <bryan.greenaway@bears.nfl.net>
2022-11-20 19:46:03 -0600

    NYJ game

--- CurrentWeek/Horizontal/script/script.js
+++ CurrentWeek/Horizontal/script/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//DET
+		//NYJ
 		$("#countdown").countdown({
-			date: "13 November 2022 12:00:00",
+			date: "27 November 2022 12:00:00",
 			format: "on"
 		});
 	});
