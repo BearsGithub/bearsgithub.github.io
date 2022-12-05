df0c63f9a226464a4566fb444f64012059026044
Bryan Greenaway <bryan.greenaway@bears.nfl.net>
2022-12-05 08:31:59 -0600

    PHI game

--- CurrentWeek/Horizontal/script/script.js
+++ CurrentWeek/Horizontal/script/script.js
@@ -68,9 +68,9 @@ $(document).ready(function(){
 			return futureFormattedDate
 		}
 		//USE 24HR MILITARY TIME
-		//GB
+		//PHI
 		$("#countdown").countdown({
-			date: "4 December 2022 12:00:00",
+			date: "18 December 2022 12:00:00",
 			format: "on"
 		});
 	});
