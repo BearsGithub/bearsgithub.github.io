36351438423bd73e5b6afff4cbc359f2bd05de66
BGreenaway <53316070+BGreenaway@users.noreply.github.com>
2025-04-14 10:46:26 -0500

    test with responsive example

---
+++ Responsive/script.js
@@ -0,0 +1,25 @@
+function countdown(date) {
+    const countDownDate = new Date(date).getTime();
+
+    const x = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
+countdown("Dec 31, 2025 23:59:59");
